60ce0b61a35c9a149c3b6c4876bd23e755e5ed9a
ZIZIBOT\User <aziziazfar95@gmail.com>
2018-10-17 13:19:14 +0800

    add Player and player attack

--- Assets/EnemyL1.cs
+++ Assets/EnemyL1.cs
@@ -8,51 +8,58 @@ public class EnemyL1 : MonoBehaviour {
 	public float moveSpeed = 0.05f;
 	public float wallDamage = 10.0f;
 
-	public bool isDead = false;
+	public bool shouldMove = true;
 
-	GameObject wall;
+	public bool isDead = false;
 
 	public float attackRestTime = 10.0f;
 
 	public Vector2 centerOfMap = new Vector2 (0, 0);
 
 	public void Update() {
-		transform.position = Vector2.MoveTowards (new Vector2 (transform.position.x, transform.position.y), centerOfMap, moveSpeed * Time.deltaTime);
-	}
-
-	public void OnTriggerEnter2D (Collider2D other){
-		if (other.gameObject.tag == "Wall") {
-			wall = other.gameObject;
-			StartCoroutine ("AttackWall");
-			//other.gameObject.GetComponent<Wall> ().DamageToWall (wallDamage);
-			//Destroy(other.gameObject);
-		}
-	}
-	public void OnTriggerExit2D (Collider2D other){
-		if (other.gameObject.tag == "Wall") {
-			StopCoroutine ("AttackWall");
-			//other.gameObject.GetComponent<Wall> ().DamageToWall (wallDamage);
+		if (shouldMove) {
+			transform.position = Vector2.MoveTowards (new Vector2 (transform.position.x, transform.position.y), centerOfMap, moveSpeed * Time.deltaTime);
 		}
 	}
 
-	IEnumerator AttackWall()  {
-		while (true) {
-			wall.GetComponent<Wall> ().DamageToWall(wallDamage);
-			Debug.Log ("Wall is under siege!");
-			yield return new WaitForSeconds (attackRestTime);
-		}
-	}
-
-	//why are the enemies not taking damage
 	public void TakeDamage(float amount) {
 		health -= amount;
 		if (health <= 0.0f) {
 			Die ();
 		}
 	}
-
+	
 	void Die() {
 		Destroy (gameObject);
 		isDead = true;
 	}
+//	public void OnTriggerEnter2D (Collider2D other){
+//		if (other.gameObject.tag == "Wall") {
+//			wall = other.gameObject;
+//			StartCoroutine ("AttackWall");
+//			//other.gameObject.GetComponent<Wall> ().DamageToWall (wallDamage);
+//			//Destroy(other.gameObject);
+//		}
+//	}
+//	public void OnTriggerExit2D (Collider2D other){
+//		if (other.gameObject.tag == "Wall") {
+//			StopCoroutine ("AttackWall");
+//			//other.gameObject.GetComponent<Wall> ().DamageToWall (wallDamage);
+//		}
+//	}
+//
+//	IEnumerator AttackWall()  {
+//		while (true) {
+//			wall.GetComponent<Wall> ().DamageToWall(wallDamage);
+//			Debug.Log ("Wall is under siege!");
+//			yield return new WaitForSeconds (attackRestTime);
+//		}
+//	}
+
+	public void AttackWall(GameObject wall) {
+		wall.GetComponent<Wall> ().DamageToWall(wallDamage);
+		Debug.Log ("Wall is under siege!");
+	}
+
+	//why are the enemies not taking damage
 }
---
+++ Assets/Player.cs
@@ -0,0 +1,54 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+//	public float moveSpeed = 1.0f;
+//	void Update() {
+//		faceMouse ();
+//	}
+//
+//	void faceMouse(){
+//		Vector3 mousePosition = Input.mousePosition;
+//		mousePosition = Camera.main.ScreenToWorldPoint (mousePosition);
+//
+//		Vector2 direction = new Vector2 (
+//		    mousePosition.x - transform.position.x,
+//			mousePosition.y - transform.position.y
+//		);
+//
+//		transform.up = direction;
+//
+//		if (Input.GetKey (KeyCode.W)) {
+//			this.transform.Translate (transform.up * moveSpeed);
+//		}
+//	}
+
+//to control and rotate using 'wasd'
+	public float moveSpeed = 1;
+	public float rotateSpeed = 12;
+	public float playerDamage = 10;
+	
+	// Update is called once per frame
+	void FixedUpdate () {
+		float moveVector = Input.GetAxis ("Vertical");
+		float rotateVector = Input.GetAxis ("Horizontal");
+
+		this.transform.Translate (0f, moveVector * moveSpeed * Time.deltaTime, 0f);
+		this.transform.Rotate (0f, 0f, rotateVector * (rotateSpeed * 10) * Time.deltaTime);
+	}
+
+//	void OnTriggerEnter2D (Collider2D target) {
+//		if (target.gameObject.tag == "Enemy") {
+//			Attack (target.gameObject);
+//		}
+//	}
+//
+//	void Attack(GameObject target){
+//		if (Input.GetKey (KeyCode.Space)) {
+//			target.transform.GetComponent<EnemyL1> ().TakeDamage (playerDamage);
+//		}
+//	}
+		
+}
