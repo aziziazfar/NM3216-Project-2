8eda7070a80c06836b42a97975e794a6eec4537d
ZIZIBOT\User <aziziazfar95@gmail.com>
2018-11-02 02:48:14 +0800

    add "E to interact" POPUP

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using UnityEngine.UI;
 
 public class Player : MonoBehaviour {
 
@@ -20,9 +21,23 @@ public class Player : MonoBehaviour {
 	public static int gold;
 	public int initialGold = 0;
 
+	[SerializeField]
+	private Image UIPopup;
+	[SerializeField]
+	private GameObject Pop;
+
+	private Image instance;
+	private GameObject canvas;
+
+	private bool shouldDestroy = false;
+
 	void Awake(){
 		gold = initialGold;
+		canvas = GameObject.Find ("Canvas");
 	}
+//	void Start(){
+//		StartCoroutine (DestroyPopUps(instance));
+//	}
 
 	public static void UpdateGold(int newGold){
 		gold += newGold;	
@@ -47,15 +62,47 @@ public class Player : MonoBehaviour {
 			//this.transform.Translate (0f, moveVector * moveSpeed * Time.deltaTime, 0f);
 
 		//this.transform.Rotate (0f, 0f, rotateVector * (rotateSpeed * 10) * Time.deltaTime);
+	IEnumerator DestroyPopUps (Image target){
+		Debug.Log ("COROTUINE STARTED");
+		yield return new WaitForSeconds (1);
 
-//	void OnTriggerEnter2D (Collider2D target) {
-//		if (target.gameObject.tag == "Enemy") {
-//			Attack (target.gameObject);
-//		}
-//	}
+		if (shouldDestroy) {
+			Destroy (target);
+		}
+	}
+	void OnTriggerEnter2D (Collider2D target) {
+		if (target.gameObject.tag == "Turret") {
+//			Debug.Log ("Interact POPUP");
+//			GameObject item = Instantiate (Pop);
+//			Vector2 playerPos = new Vector2 (transform.position.x, transform.position.y);
 //
+//			item.transform.SetParent (transform, false);
+//			item.transform.position = playerPos;
+			if (instance == null)
+				CreatePopUp ();
+
+			shouldDestroy = true;
+			StartCoroutine (DestroyPopUps(instance));
+			StopCoroutine (DestroyPopUps (instance));
+		}
+	}
+
+	void OnTriggerExit2D (Collider2D target) {
+		if (target.gameObject.tag == "Turret") {
+			//Destroy (instance);
+		}
+	}
+
 	void Attack(GameObject target){
 		target.transform.GetComponent<EnemyL1> ().TakeDamage (playerDamage);
 	}
+	void CreatePopUp(){
+		instance = Instantiate (UIPopup);
+		Vector2 playerPos = new Vector2 (transform.position.x, transform.position.y);
+
+		instance.transform.SetParent (canvas.transform, false);
+		instance.transform.position = playerPos;
+	}
+
 }
 //	
